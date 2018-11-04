cdbec2be39bb00b1d3453a57aff3ad9d05a51164
Sergei <sergeyfk05@gmail.com>
2018-11-04 18:50:56 +0300

    add LocalizeManager and resxProvider

---
+++ MusicApp/Localize/ResxLocalizationProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using MusicApp.Resources.Languages;
+
+namespace MusicApp.Localization
+{
+    /// <summary>
+    /// Реализация поставщика локализованных строк через ресурсы приложения
+    /// </summary>
+    public class ResxLocalizationProvider : ILocalizationProvider
+    {
+        private IEnumerable<CultureInfo> _cultures;
+
+        public object Localize(string key)
+        {
+            return Language.ResourceManager.GetObject(key);
+        }
+
+        public IEnumerable<CultureInfo> Cultures => _cultures ?? (_cultures = new List<CultureInfo>
+        {
+            new CultureInfo("ru-RU"),
+            new CultureInfo("en-US"),
+        });
+    }
+}
